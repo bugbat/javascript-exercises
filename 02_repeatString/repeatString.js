077b25d25627a1f0129df6ec8f4f38d679501513
bugbat <85291787+bugbat@users.noreply.github.com>
2024-03-29 12:56:01 -0400

    Add my answers to first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeats) {
+  if (repeats < 0) return "ERROR";
+  let repeatedWord = "";
+  for (let i = 0; i < repeats; i++) {
+    repeatedWord += word;
+  }
+  return repeatedWord;
 };
 
 // Do not edit below this line
