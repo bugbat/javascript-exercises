44bec9560fd3ecacfcbc2026fe723e25e6cd916d
bugbat <85291787+bugbat@users.noreply.github.com>
2024-04-02 21:43:17 -0400

    add answers for 10-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  
+  return people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    return oldestAge < currAge ? current : oldest;
+  });
 };
+
+function getAge(birth, death) {
+  const currYear = new Date().getFullYear();
+  if (!death) {
+    death = currYear;
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
